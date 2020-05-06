4ed329cb2e06184968819fe0b6ef69575d407966
Gabriel Gamero <gabriel.gamero@pucp.pe>
2020-05-06 04:18:30 -0500

    programa acabado

--- src/proyectoprueba/ProyectoPrueba.java
+++ src/proyectoprueba/ProyectoPrueba.java
@@ -25,13 +25,24 @@ public class ProyectoPrueba {
             System.out.println("2. Restar");
             Scanner sc = new Scanner(System.in);
             String op = sc.nextLine();
+            
+            System.out.print("Ingrese el primer numero: ");
+            String strn1 = sc.nextLine();
+            
+            System.out.print("Ingrese el segundo numero: ");
+            String strn2 = sc.nextLine();
+            
+            int n1 = Integer.valueOf(strn1);
+            int n2 = Integer.valueOf(strn2);
 
             switch (op) {
                 case "1":
-
+                    int resultado1 = sumar(n1, n2);
+                    System.out.println("La suma es: " + resultado1);
                     break;
                 case "2":
-
+                    int resultado2 = funcionRestar(n1,n2);
+                    System.out.println("La suma es: " + resultado2);
                     break;
             }
 
@@ -39,7 +50,7 @@ public class ProyectoPrueba {
 
     }
 
-    int sumar(int a, int b) {
+    static int sumar(int a, int b) {
         int res = 0;
         res = a + b;
 
@@ -48,7 +59,7 @@ public class ProyectoPrueba {
     }
 
 
-    int funcionRestar(int num1, int num2){
+    static int funcionRestar(int num1, int num2){
 
         int result;
         result = num1 - num2;
